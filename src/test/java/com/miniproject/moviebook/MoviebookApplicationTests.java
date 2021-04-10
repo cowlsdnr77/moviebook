6843f6437aa79fb8cf3931509f1700475b96a9af
jinook <cowlsdnr77@gmail.com>
2021-04-10 16:42:09 +0900

    Initial commit

---
+++ src/test/java/com/miniproject/moviebook/MoviebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniproject.moviebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

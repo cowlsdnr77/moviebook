a0bd22928e603f0859efa54b0ed8d4addfc33f6f
jinook <cowlsdnr77@gmail.com>
2021-04-14 16:09:31 +0900

    회원가입 기능 commit

--- src/main/java/com/miniproject/moviebook/service/UserService.java
+++ src/main/java/com/miniproject/moviebook/service/UserService.java
@@ -6,6 +6,8 @@ import com.miniproject.moviebook.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -14,8 +16,15 @@ public class UserService {
 //    private final BCryptPasswordEncoder bCryptPasswordEncoder; 스프링 시큐리티 설치해야 사용가능
 
     /* 회원가입 */
-    public User createUser(SignupRequestDto signupRequestDto) {
-        User user = new User(signupRequestDto);
+    public User createUser(SignupRequestDto requestDto) {
+
+        String username = requestDto.getUsername();
+        Optional<User> found = userRepository.findByUsername(username);
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자 ID 가 존재합니다.");
+        }
+
+        User user = new User(requestDto);
         return userRepository.save(user);
     }
 }
